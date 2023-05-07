5e12a28b825bab2298a2c2aa678748ad8ee80581
Aleksey <meysonsib200189@mail.ru>
2023-05-07 16:41:22 +0700

    заменил views ResultFragment на compose элементы сделал вызов в onCreateView ComposeView ResultFragmentContent

--- app/src/main/java/com/bignerdranch/android/basicgame/ResultFragment.kt
+++ app/src/main/java/com/bignerdranch/android/basicgame/ResultFragment.kt
@@ -1,7 +1,6 @@
 package com.bignerdranch.android.basicgame
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -14,18 +13,16 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.sp
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.findNavController
-import com.bignerdranch.android.basicgame.databinding.FragmentResultBinding
 import com.bignerdranch.android.basicgame.viewmodel.ResultViewModel
 import com.bignerdranch.android.basicgame.viewmodel.factory.ResultViewModelFactory
 
 class ResultFragment : Fragment() {
-
-    private var _binding: FragmentResultBinding? = null
-    private val binding get() = _binding!!
 
     lateinit var viewModel: ResultViewModel
     lateinit var viewModelFactory: ResultViewModelFactory
@@ -34,9 +31,13 @@ class ResultFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        _binding = FragmentResultBinding.inflate(inflater, container, false).apply {
-            //ДОБАВИЛ ComposeView (предварительно в xml внес его)
-            composeView.setContent {
+        val result = ResultFragmentArgs.fromBundle(requireArguments()).result
+        viewModelFactory = ResultViewModelFactory(result)
+        viewModel = ViewModelProvider(this, viewModelFactory)
+            .get(ResultViewModel::class.java)
+
+        return ComposeView(requireContext()).apply {
+            setContent {
                 MaterialTheme {
                     Surface {
                         view?.let { ResultFragmentContent(view = it, viewModel = viewModel) }
@@ -44,20 +45,10 @@ class ResultFragment : Fragment() {
                 }
             }
         }
-
-        val view = binding.root
-        val result = ResultFragmentArgs.fromBundle(requireArguments()).result
-        viewModelFactory = ResultViewModelFactory(result)
-        viewModel = ViewModelProvider(this, viewModelFactory)
-            .get(ResultViewModel::class.java)
-        binding.resultViewModel = viewModel
-        click(binding, view)
-        return view
     }
 
     @Composable
     fun ResultFragmentContent(view: View, viewModel: ResultViewModel) {
-//Todo место для кода составных элементов своего рода контейнер перед отправкой в onCreateView compose.setContent{}
         Column(
             modifier = Modifier.fillMaxWidth(),
             horizontalAlignment = Alignment.CenterHorizontally
@@ -79,16 +70,5 @@ class ResultFragment : Fragment() {
     @Composable
     fun ResultText(result: String) {
         Text(text = result, fontSize = 28.sp, textAlign = TextAlign.Center)
-    }
-
-    fun click(binding: FragmentResultBinding, view: View) {
-        binding.newGameButton.setOnClickListener {
-            view.findNavController().navigate(R.id.action_resultFragment_to_gameFragment)
-        }
-    }
-
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
     }
 }
